4ee6e88201306b6fc4bf4c4d5fa28a3783708c4a
Saalimkhan7 <saalimk110@gmail.com>
2024-09-22 08:39:03 -0700

    ThemeChanger

---
+++ 09contextapi/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Scan all JS, JSX, TS, and TSX files inside src
+  ],
+  darkMode:"class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
